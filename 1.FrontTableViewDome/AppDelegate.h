c68b7d757c8fd0624cc8648c500a0159151f7787
张文健 <zhangwenjian@zhangwenjiandeMacBook-Pro.local>
2014-03-05 17:08:54 +0800

    Initial Commit

---
+++ 1.FrontTableViewDome/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  1.FrontTableViewDome
+//
+//  Created by 张文健 on 14-3-5.
+//  Copyright (c) 2014年 张文健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 1.FrontTableViewDome/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  1.FrontTableViewDome
+//
+//  Created by 张文健 on 14-3-5.
+//  Copyright (c) 2014年 张文健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
